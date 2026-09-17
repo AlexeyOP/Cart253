7a8e79dff54c0fb229a01a9268baf1154d03a59f
AlexeyOP <a_openko@live.concordia.ca>
2026-09-17 16:05:24 -0400

    trying the colour chage

--- topics/challenge/instructions-challenge/js/script.js
+++ topics/challenge/instructions-challenge/js/script.js
@@ -12,7 +12,7 @@
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
-
+    createCanvas(640, 640);
 }
 
 
@@ -20,5 +20,6 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
+    background(95, 123, 237)
 
 }
