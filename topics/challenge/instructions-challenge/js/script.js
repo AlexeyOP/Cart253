c41d85db994195c3062f5abc3c649dc5773d7aed
AlexeyOP <a_openko@live.concordia.ca>
2026-09-17 16:22:28 -0400

    tries of doing draws

--- topics/challenge/instructions-challenge/js/script.js
+++ topics/challenge/instructions-challenge/js/script.js
@@ -23,3 +23,14 @@ function draw() {
     background(95, 123, 237)
 
 }
+function draw() {
+    function setup() {
+        createCanvas(100, 100);
+
+        background(200);
+
+        arc(50, 50, 80, 80, 0, PI + HALF_PI);
+
+        describe('A white circle on a gray canvas. The top-right quarter of the circle is missing.');
+    }
+}
