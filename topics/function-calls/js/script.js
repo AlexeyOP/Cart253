a548f676973792c00d293320ed32bd014fb0c91f
AlexeyOP <a_openko@live.concordia.ca>
2026-09-18 13:57:49 -0400

    added a recctangle

--- topics/function-calls/js/script.js
+++ topics/function-calls/js/script.js
@@ -22,3 +22,14 @@ function setup() {
 function draw() {
     background(219, 119, 13);
 }
+
+function setup() {
+    createCanvas(100, 100);
+
+    background(200);
+
+    // Give all corners a radius of 20.
+    rect(30, 20, 55, 50, 20);
+
+    describe('A white rectangle with a black outline and round edges on a gray canvas.');
+}
