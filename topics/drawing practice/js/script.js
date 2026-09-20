8b900b35cc2496a14dbbc75f714f457fd6df9a16
AlexeyOP <a_openko@live.concordia.ca>
2026-09-20 15:09:20 -0400

    create canvas

--- topics/drawing practice/js/script.js
+++ topics/drawing practice/js/script.js
@@ -1,6 +1,6 @@
 /**
- * Title of Project
- * Author Name
+ * drawing practice
+ * Alexey Openkov
  * 
  * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
  * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
@@ -20,5 +20,10 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
+// a nice square canvas to work with
+ createcanvas(640,640);
+}
 
+function draw() {
+    
 }
