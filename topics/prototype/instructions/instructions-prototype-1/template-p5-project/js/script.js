0476c518095c2ec3091a743f2594d75f19d636e3
AlexeyOP <a_openko@live.concordia.ca>
2026-09-22 17:04:18 -0400

    path to image

--- topics/prototype/instructions/instructions-prototype-1/template-p5-project/js/script.js
+++ topics/prototype/instructions/instructions-prototype-1/template-p5-project/js/script.js
@@ -15,7 +15,7 @@ function setup() {
 }
 
 function draw() {
-  backgroundImage.src=image/moon.jpg;
+  backgroundImage.src=images/moon.jpg;
   
   // Center the robot in the canvas
   translate(width / 2, height / 2);
