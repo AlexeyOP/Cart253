8907fbe80f4fd1b5bd19b4cb23e2531094847f2c
AlexeyOP <a_openko@live.concordia.ca>
2026-09-22 17:11:26 -0400

    change path

--- topics/prototype/instructions/instructions-prototype-1/template-p5-project/js/script.js
+++ topics/prototype/instructions/instructions-prototype-1/template-p5-project/js/script.js
@@ -15,7 +15,7 @@ function setup() {
 }
 
 function draw() {
-  document.body.style.backgroundImage=images/moon.jpg;
+  document.body.style.backgroundImage=assets/images/moon.jpg;
   
   // Center the robot in the canvas
   translate(width / 2, height / 2);
