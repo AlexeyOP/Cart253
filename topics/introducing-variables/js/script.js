14b92cfd40f1fbadad8cbc3d5f3e6f5df5edff80
AlexeyOP <a_openko@live.concordia.ca>
2026-09-23 18:40:32 -0400

    introducing variables script changes

--- topics/introducing-variables/js/script.js
+++ topics/introducing-variables/js/script.js
@@ -12,7 +12,8 @@
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
-
+// Create the canvas
+    createCanvas(640, 640);
 }
 
 
@@ -20,5 +21,12 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
-
+background(0);
+    
+    // Draw a circle in the centre of the canvas
+    push();
+    noStroke();
+    fill(255, 255, 0);
+    ellipse(320, 320, 100, 100);
+    pop();
 }
