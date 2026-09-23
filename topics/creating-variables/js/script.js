ae7ee577c9ce696f17652046bd1b40b92c6e3c7f
AlexeyOP <a_openko@live.concordia.ca>
2026-09-23 18:26:14 -0400

    draw a cheese with a hole

--- topics/creating-variables/js/script.js
+++ topics/creating-variables/js/script.js
@@ -12,13 +12,27 @@
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
-
+ // Create the canvas
+    createCanvas(480, 480);
 }
+
 
 
 /**
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
+    // Cheese colour (yellow)
+    background(255, 255, 0);
+
+     // Draw a hole in the upper left
+    push();
+    noStroke();
+    fill(0);
+    // When we only provide a width argument we get a circle
+    // with that diameter
+    // https://p5js.org/reference/p5/ellipse/
+    ellipse(140, 175, 180);
+    pop();
 
 }
